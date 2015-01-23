67c8e6a691b59bdb98b6739d9d1aa4bb51dab815
Till Niese <info@tn-dev.com>
2015-01-23 08:44:22 +0100

    code cleanup:
    - removed not required variables
    - created function for common code

--- jquery.scroll-reason.js
+++ jquery.scroll-reason.js
@@ -1,63 +1,84 @@
 (function(jQuery, undefined) {
+  
   var $ = jQuery,
-    scrollTopOld = jQuery.fn.scrollTop;
+      scrollTopOld = jQuery.fn.scrollTop,
+      dataKey = 'scroll-event';
+      
+  function getScrollInfo($elm) {
+    var info = $elm.data(dataKey) || {};
+    $elm.data(dataKey, info);
+    return info;
+  }
 
+  
   function mappedElement(elm) {
-
     if (elm === document || elm === window || elm.tagName === 'HTML' || elm.tagName === 'BODY') {
       elm = document;
     }
-
+    
     return $(elm);
   }
 
   function scrollFilter(event, originalEvent) {
-    var $elm = mappedElement(event.target);
+    var $elm = mappedElement(event.target),
+        info = getScrollInfo($elm),
+        scrollTop = $elm.scrollTop(),
+        diff = Math.floor(info.expectedScrollPos - scrollTop);
 
-
-    var expectedScroll = $elm.data('event-scroll-expected');
-    var scrollTop = $elm.scrollTop();
-    var diff = Math.floor(expectedScroll - scrollTop);
-    var scrollTopSet = $elm.data('event-scroll-set');
-
-    if (scrollTopSet && diff == 0) {
+    if (info.scrollSet && diff === 0) {
       event.scrollType = 'programm';
     } else {
       event.scrollType = 'user';
     }
 
-    $elm.data('event-scroll-set', false);
+    info.scrollSet = false;
 
     return event;
   };
 
+
   jQuery.event.fixHooks.scroll = jQuery.event.fixHooks.scroll || {};
   jQuery.event.fixHooks.scroll.filter = scrollFilter;
 
-  jQuery.Tween.propHooks.scrollTop = jQuery.Tween.propHooks.scrollLeft = {
+  //TODO we need scrollLeft support
+  jQuery.Tween.propHooks.scrollTop = /*jQuery.Tween.propHooks.scrollLeft =*/ {
     set: function(tween) {
+      var $elm, info;
+      
       if (tween.elem.nodeType && tween.elem.parentNode) {
-        var $elm = mappedElement(tween.elem);
-        $elm.data('event-scroll-set', true);
+        $elm = mappedElement(tween.elem);
+        info = getScrollInfo($elm);
+        
+        info.scrollSet = true;
+
+        /*
+          Only set the expected value if start and end value are different.
+        
+          This is used to support scroll animations on html/body as they both map do document.
+          $('body, html').animate({scrollTop:x},1000);
+        */
         if (tween.start !== tween.end) {
-          //we only need to do something of the values differ
-          //this is a hack for the $('body, html').animate({scrollTop:x},1000);
-          $elm.data('event-scroll-expected', tween.now);
+          info.expectedScrollPos = tween.now;
         }
+        
         tween.elem[tween.prop] = tween.now;
       }
     }
   };
 
+
   jQuery.fn.scrollTop = function() {
 
     if (arguments.length === 0) {
       this.each(function(elm) {
-        var $elm = mappedElement(elm);
-        $elm.data('event-scroll-set', true);
+        var $elm = mappedElement(elm),
+            info = getScrollInfo($elm);
+        
+        info.scrollSet = true;
       });
     }
 
     return scrollTopOld.apply(this, arguments);
   };
+  
 }(jQuery));
